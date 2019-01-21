0a666605bccbaffc8e3e8d873f28f77daf3e4b46
dengtongcai <dengtongcai@gmail.com>
2019-01-21 18:38:35 +0800

    多配置文件，日志配置

--- 02-spring-boot-profiles/src/main/java/me/datoucai/profiles/ProfilesApplication.java
+++ 02-spring-boot-profiles/src/main/java/me/datoucai/profiles/ProfilesApplication.java
@@ -9,6 +9,5 @@ public class ProfilesApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProfilesApplication.class, args);
     }
-
 }
 
--- 02-spring-boot-profiles/src/main/java/me/datoucai/profiles/api/ProfilesController.java
+++ 02-spring-boot-profiles/src/main/java/me/datoucai/profiles/api/ProfilesController.java
@@ -1,5 +1,7 @@
 package me.datoucai.profiles.api;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author cc
  * @date 2019/1/17
  */
+@Slf4j
 @RestController
 public class ProfilesController {
+    @Value("${config.level}")
+    private String level;
 
     @GetMapping(value = "/profiles", produces = "application/json;charset=utf-8")
     public String profiles() {
-        return "hello profiles !!!";
+        String res = level;
+
+        log.info("level is {}", level);
+        log.warn("level is {}", level);
+        log.error("level is {}", level);
+        return level;
     }
 }
